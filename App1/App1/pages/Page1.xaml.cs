764f5dcc1d5c1b9d47063f98bb5b9eabd3b6faff
Adel_ <Adel_@DESKTOP-64HRO07>
2022-02-12 00:33:25 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/pages/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using App1.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        private viewmodel1 viewmodel1 = new viewmodel1();
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = viewmodel1;
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await viewmodel1.ChangeNumber();
+        }
+    }
+}
---
+++ App1/App1/viewmodels/viewmodel1.cs
@@ -0,0 +1,47 @@
+﻿using App1.pages;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1.viewmodels
+{
+    public class viewmodel1:INotifyPropertyChanged
+    {
+        private int _number = 38;
+
+        public int Number
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task ChangeNumber()
+        {
+            await Task.Delay(1000);
+            int random = new Random().Next(300);
+            Number = random;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ICommand Label1Command => new Command(async value =>
+        {
+            await ChangeNumber();
+        });
+
+        public ICommand command => new Command(async value =>
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new Page2());
+        });
+    }
+}
